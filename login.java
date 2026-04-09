ee3069d11ac8fe54e7885af3ce30d98126e7697d
sadviq-badri <sadviqch@gmail.com>
2026-04-09 17:58:27 +0530

    Create login class with a loop to print numbers
    Fix syntax errors in the login class.

---
+++ login.java
@@ -0,0 +1,13 @@
+Public class login
+{
+   public static void main()
+   {
+       int i =0;
+
+       for (i=0;i<==15;i++)
+
+       {
+            System.out Println ("Number: "+i);
+       }
+   }
+}
