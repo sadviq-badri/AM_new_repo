3f2b137bc3c921244392baa8bc43adb78fa9f3e1
sadviq-badri <sadviqch@gmail.com>
2026-04-10 12:02:14 +0530

    Update login.java

---
+++ login.java
@@ -0,0 +1,13 @@
+Public class login
+{
+   public static void main()
+   {
+       int i =0;
+
+       for (i=0;i<==20;i++)
+
+       {
+            System.out Println ("Number: "+i);
+       }
+   }
+}
